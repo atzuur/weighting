4a0e73caaceb0d21733b7b2fae4c8f0c59cc5488
atzuur <johannes.pihko@gmail.com>
2023-06-01 15:12:34 +0300

    fixes + cleanup

--- weighting.py
+++ weighting.py
@@ -11,16 +11,17 @@ Reference:
 import math
 import warnings as w
 from numbers import Number
+from typing import Iterable
 
 
-def normalize(weights: list):
+def normalize(weights: Iterable[Number]):
     """
-    Normalize a list of weights to sum to 1
+    Normalize a list of numbers to sum up to 1
     """
 
     if min(weights) < 0:
-        absmin = abs(min(weights))
-        weights = [w + absmin + 1 for w in weights] # remove negative weights
+        abs_min = abs(min(weights))
+        weights = [w + abs_min + 1 for w in weights] # remove negative weights
 
     tot = sum(weights)
     return [w / tot for w in weights]
@@ -41,16 +42,14 @@ def ascending(frames: int):
     """
     Linear ascending curve
     """
-    val = [x for x in range(1, frames + 1)]
-    return normalize(val)
+    return normalize(range(1, frames + 1))
 
 
 def descending(frames: int):
     """
     Linear descending curve
     """
-    val = [x for x in range(frames, 0, -1)]
-    return normalize(val)
+    return normalize(range(frames, 0, -1))
 
 
 def equal(frames: int):
@@ -60,12 +59,12 @@ def equal(frames: int):
     return [1 / frames] * frames
 
 
-def gaussian(frames: int, apex: Number = 2, std_dev: Number = 1, bound: tuple[float, float] = (0, 2)):
+def gaussian(frames: int, apex: Number = 2, std_dev: Number = 1, bound: tuple[Number, Number] = (0, 2)):
     """
     Args:
         bound: `[a, b]` | x axis vector from `a` to `b`
         apex: `μ`       | the position of the center of the peak, relative to x axis vector
-        std_dev: `σ`    | width of the "bell", higher == broader / flatter
+        std_dev: `σ`    | width of the "bell", higher == broader/flatter
     Reference:
         https://en.wikipedia.org/wiki/Gaussian_function
     """
@@ -80,14 +79,14 @@ def gaussian(frames: int, apex: Number = 2, std_dev: Number = 1, bound: tuple[fl
     return normalize(val)
 
 
-def gaussian_sym(frames: int, std_dev: Number = 1, bound: tuple[float, float] = (0, 2)):
+def gaussian_sym(frames: int, std_dev: Number = 1, bound: tuple[Number, Number] = (0, 2)):
     """
     Same as `gaussian()` but symmetric;
     the peak (apex) will always be at the center of the curve
     """
     _warn_bound(bound, "gaussian_sym")
 
-    max_abs = max(bound)
+    max_abs = max(map(abs, bound[:2]))
     r = scale_range(frames, -max_abs, max_abs)
 
     val = [1 / (math.sqrt(2 * math.pi) * std_dev)
@@ -118,7 +117,7 @@ def func_eval(func: str, nums: list[float]):
     """
 
     # math functions + math related builtins
-    namespace = {k:v for k, v in math.__dict__.items() if not k.startswith("_")}
+    namespace = {k: v for k, v in math.__dict__.items() if not k.startswith("_")}
     namespace |= {
         'frames': len(nums), # total number of items (frames)
         'x': None, # iterator for nums
@@ -137,7 +136,7 @@ def func_eval(func: str, nums: list[float]):
     return eval(f"[({func}) for x in {nums}]", namespace)
 
 
-def custom(frames: int, func: str = "", bound: tuple[float, float] = (0, 1)):
+def custom(frames: int, func: str = "", bound: tuple[Number, Number] = (0, 1)):
     """
     Arbitrary custom weighting function
     """
@@ -162,11 +161,12 @@ def divide(frames: int, weights: list[float]):
     return normalize(val)
 
 
-def _warn_bound(bound: tuple, func: str):
+def _warn_bound(bound: tuple, func_name: str):
     if len(bound) < 2:
-        raise ValueError(f"{func}: bound must be a tuple of length 2, got {bound}")
+        raise ValueError(f"{func_name}: bound must be a tuple of length 2, got {bound}")
     elif len(bound) > 2:
-        w.warn(f"{func}: bound was given as a tuple of length {len(bound)}, only the first two values will be used",
+        w.warn(f"{func_name}: bound was given as a tuple of length {len(bound)}, "
+               "only the first two values will be used",
                RuntimeWarning)
 
 
